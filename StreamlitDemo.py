497f4efdc3e1359fc8a4b0e9fe076a84f379bfe2
keetane <yu.takahashi.k@gmail.com>
2023-11-28 22:13:52 +0900

    rdkit was described in demo file

--- StreamlitDemo.py
+++ StreamlitDemo.py
@@ -1,11 +1,12 @@
 import streamlit as st
-# from rdkit import Chem
-# from rdkit.Chem import Draw, rdDepictor
-# # 環状化合物をきれいに描画 https://future-chem.com/rdkit-coordgen/
-# rdDepictor.SetPreferCoordGen(True)
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import Draw, rdDepictor
+# 環状化合物をきれいに描画 https://future-chem.com/rdkit-coordgen/
+rdDepictor.SetPreferCoordGen(True)
 
 
 
 # Header
 st.title("Retro-inversian")
-st.write("")
+st.write("rdkit version", rdkit.__version__)
