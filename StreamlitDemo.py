341d684ec9f2a2e23cf503311e6b07a839080b2d
keetane <yu.takahashi.k@gmail.com>
2023-11-28 22:07:10 +0900

    repository created

---
+++ StreamlitDemo.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# from rdkit import Chem
+# from rdkit.Chem import Draw, rdDepictor
+# # 環状化合物をきれいに描画 https://future-chem.com/rdkit-coordgen/
+# rdDepictor.SetPreferCoordGen(True)
+
+
+
+# Header
+st.title("Retro-inversian")
+st.write("")
