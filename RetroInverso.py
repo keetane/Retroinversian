ff6f4c9c2628f057864e26f9d435c90609897de0
keetane <yu.takahashi.k@gmail.com>
2023-11-28 22:56:44 +0900

    updated

--- RetroInverso.py
+++ RetroInverso.py
@@ -2,7 +2,7 @@ import streamlit as st
 from rdkit import Chem
 from rdkit.Chem import Draw, rdDepictor
 # 環状化合物をきれいに描画 https://future-chem.com/rdkit-coordgen/
-# rdDepictor.SetPreferCoordGen(True)
+rdDepictor.SetPreferCoordGen(True)
 
 
 
