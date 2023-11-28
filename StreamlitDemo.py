6d37e244c7036e42fa862c4a9a0f9e67a9d21cca
keetane <yu.takahashi.k@gmail.com>
2023-11-28 22:22:37 +0900

    minor updated

--- StreamlitDemo.py
+++ StreamlitDemo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import rdkit
-# from rdkit import Chem
-# from rdkit.Chem import Draw, rdDepictor
-# # 環状化合物をきれいに描画 https://future-chem.com/rdkit-coordgen/
+from rdkit import Chem
+from rdkit.Chem import Draw, rdDepictor
+# 環状化合物をきれいに描画 https://future-chem.com/rdkit-coordgen/
 # rdDepictor.SetPreferCoordGen(True)
 
 
